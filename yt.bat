476c6c08351d509038488c19cac1f81dfd9c4a6e
spyspace66 <34733366+spyspace66@users.noreply.github.com>
2021-01-23 22:54:35 +0700

    Add description

--- yt.bat
+++ yt.bat
@@ -1,18 +1,10 @@
-# Youtube Downloader Playlist
- Download playlist as audio or video
-
 @echo off
 set /p link=Enter Playlist URL  
 set /p type=Audio or Video (0 : audio, 1 : video) 
 set /p folder=Folder Name 
-
 if %type% == 0 (
   G:
   cd G:\Youtube To Watch\YTAudio
-
-#Change to your drive, and folder in that drive
-
-
   mkdir "%folder%"
   cd "%folder%"
   youtube-dl --ignore-errors --format bestaudio --extract-audio --audio-format mp3 --audio-quality 160K --yes-playlist %link%
